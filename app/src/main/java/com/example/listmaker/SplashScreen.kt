fab8ce5e385adf47a0e825d7478f2ed6c44bea71
Mohammed <mohamed.amin.2290@gmail.com>
2022-07-22 21:56:36 +0200

    App Icon and Splash Screen

---
+++ app/src/main/java/com/example/listmaker/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.listmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        splash.alpha = 0f
+        splash.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this,  MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in , android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
